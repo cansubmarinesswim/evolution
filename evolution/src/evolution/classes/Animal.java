622e2b3afbbc9a4ca1090ced5717778ec63e1422
Tomasz Kurkowski <tomasz.kurkowski@techmo.pl>
2021-03-15 20:42:34 +0100

    Added parameters.json, DNA class, Animal class

---
+++ evolution/src/evolution/classes/Animal.java
@@ -0,0 +1,19 @@
+package evolution.classes;
+
+public class Animal {
+
+//	- id zwierzęcia  
+//	- koordynat x  
+//	- koordynat y  
+//	- kierunek zwierzęcia  
+//	- energia  
+//	- kod genetyczny (niezmienny dla osobnika)  
+//	- boolean opisujący czy zyje czy nie  
+//	- tablica z id dzieci
+	
+	public int ID;
+	public int currentEnergy;
+	public int startingEnergy;
+	public Dna dna;
+	
+}
